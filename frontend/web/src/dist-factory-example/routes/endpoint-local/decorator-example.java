12f58f1dbde96ede6342185058d7d9e65f974d23
rodined <rodined@users.noreply.github.com>
2025-11-17 15:14:07 +0000

    Add controller.scss, decorator-example.java

---
+++ frontend/web/src/dist-factory-example/routes/endpoint-local/decorator-example.java
@@ -0,0 +1,4 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/secondop_fhe/frontend/web/src/dist-factory-example/routes/endpoint-local/decorator-example.java
+// Generated at: 2025-12-03T23:45:47.802Z
+
+
